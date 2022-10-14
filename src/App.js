0a9bb010b72e215b0ff0d3415ec30bfc64da88e5
sotopro <dasg.1993@gmail.com>
2022-10-13 19:09:47 -0500

    [feature] - add image

--- src/App.js
+++ src/App.js
@@ -6,19 +6,22 @@ const products = [
     id: 1,
     name: 'Product 1',
     price: 100,
-    description: 'description'
+    description: 'description',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbN4hEMNReLTBOTFa8iKdx7JSM2EJ5ZPdfJCzu0CGY_H7UDX33I8GXQil_gkw0FqmHUMY&usqp=CAU'
   },
   {
     id: 2,
     name: 'Product 2',
     price: 200,
-    description: 'description'
+    description: 'description',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqIfPm1qtVRH2dz-EMrRc3l7r8kf-mVcQfXinzXrh992OgCQD1xmpN19C0kbgYJ2YwbxY&usqp=CAU'
   },
   {
     id: 3,
     name: 'Product 3',
     price: 300,
-    description: 'description'
+    description: 'description',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-Do-eg663j3L9FuvvV8--uvnknGCUVoBE1tmLLOnST65uTF4Z_StK7dTZ2pXe0wLLReY&usqp=CAU'
   },
 ]
 
@@ -38,6 +41,7 @@ function App() {
           {products.map((product) => (
             <div key={product.id}>
               <h3>{product.name}</h3>
+              <img className='product-image' src={product.imageUrl} alt={product.name} />
               <p>{product.description}</p>
             </div>
           ))}
